7f4b404a34ac2e7c9fb13bd8d937e660fe37f80d
Lorena <lorenapim@gmail.com>
2023-10-03 22:35:38 -0300

    Slider arreglado

--- cliente/src/views/Home.jsx
+++ cliente/src/views/Home.jsx
@@ -2,31 +2,17 @@ import React, { useContext, useState } from 'react'
 import { DataContext } from '../context/DataContext';
 import CardsHome from '../components/CardsHome';
 import SliderHome from '../components/SliderHome';
-import { useEffect } from 'react';
-import axios from 'axios';
 import './style.scss';
 
 const Home = () => {
 
-    const [dataPromoted, setDataPromoted] = useState([]);
 
-    const { data, getData } = useContext(DataContext);
+    const { data } = useContext(DataContext);
     console.log(data);
-
-    const getProdutcsPromoted = async () => {
-        const data = await axios.get("http://localhost:3001/publicacionesPromoted");
-        setDataPromoted(data.data);
-    }
-
-    useEffect(() => {
-        getData()
-        getProdutcsPromoted();
-    }, [])
-
 
     return (
         <div>
-            <SliderHome data={dataPromoted} />
+            <SliderHome/>
             <div className='home'>
                 {data.map((ele) => {
                     return (
@@ -39,3 +25,4 @@ const Home = () => {
 }
 
 export default Home
+
